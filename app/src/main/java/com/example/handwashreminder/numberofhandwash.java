583ddfae22036c9a4f4ff397fe6fe71d40c33337
Kunjal Sarma <kunjalsarma.10@gmail.com>
2020-06-24 20:35:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/handwashreminder/numberofhandwash.java
@@ -0,0 +1,14 @@
+package com.example.handwashreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class numberofhandwash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numberofhandwash);
+    }
+}
